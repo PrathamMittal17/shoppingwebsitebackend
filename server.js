e438be17f0f3187d9621081178f34afd6ad18733
PrathamMittal17 <prathammittal172002@gmail.com>
2022-08-31 12:16:27 +0530

    changes

--- server.js
+++ server.js
@@ -9,19 +9,8 @@ const PORT = process.env.PORT || 5001;
 app.use(cors())
 app.use(express.json());
 
-// const db = knex({
-//     client: 'pg',
-//     connection: {
-//       host : '127.0.0.1',
-//       port : 5432,
-//       user : 'postgres',
-//       password : '1234',
-//       database : 'shoppingsite'
-      
-//     }
-//   });
 
-  process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0
 const db = knex({
                     client: 'pg',
                     connection: {
@@ -30,7 +19,7 @@ const db = knex({
                     }
                 });
 
-
+                
   
 
 app.get("/",(req,res)=>{
